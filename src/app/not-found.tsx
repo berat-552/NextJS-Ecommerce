e2567b55b68b8f68d27b87916531539f75685a52
berat-552 <berat.d7599@gmail.com>
2023-09-16 16:15:34 +0100

    error page button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,21 @@
+"use client";
 import React from "react";
 import Link from "next/link";
-
-export default async function NotFound() {
+import { useRouter } from "next/navigation";
+export default function NotFound() {
+  const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-center leading-10 min-h-screen">
       <h1 className="font-medium text-xl sm:text-2xl md:text-3xl">
         404 Not Found
       </h1>
       <p className="text-md">Please try again.</p>
-      <Link href={"/"} className="font-medium text-xl btn btn-primary">
+      <button
+        onClick={() => router.refresh()}
+        className="font-medium text-xl btn btn-primary"
+      >
         Return Home
-      </Link>
+      </button>
     </div>
   );
 }
